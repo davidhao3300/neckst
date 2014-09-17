04f2bec962399c8987ee05d8e188344fec80eb2e
William Huang <whuang6@stanford.edu>
2014-09-17 00:54:47 -0500

    parsed json in networkparser

--- Neckst/Neckst/AppDelegate.swift
+++ Neckst/Neckst/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var networkParser: NetworkParser?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.networkParser = NetworkParser()
         return true
     }
 
---
+++ Neckst/Neckst/NetworkParser.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkParser.swift
+//  Neckst
+//
+//  Created by William Huang on 9/16/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+class NetworkParser {
+    
+    var eventArray: Array<Dictionary<String, String>> = []
+    
+    init(){
+        println("Network Parser initialized.")
+        let urlPath = "http://neckst.herokuapp.com/api/events"
+        let url: NSURL = NSURL(string: urlPath)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+            if error != nil{
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            if err != nil{
+                println("JSON Error: \(err?.localizedDescription)")
+            }
+            if let element: Array<Dictionary<String, String>> = jsonResult["events"] as? Array<Dictionary<String, String>>{
+                self.eventArray = element
+                self.printArray()
+            }
+        })
+        task.resume()
+    }
+    
+    func printArray(){
+        for dict: Dictionary<String, String> in self.eventArray{
+            println("NEW DICTIONARY: ")
+            for (key, value) in dict{
+                println("key: \(key), value: \(value)")
+            }
+        }
+    }
+}
