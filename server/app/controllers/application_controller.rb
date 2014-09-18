330101ec47e17586a7635288ae53dc955faf445a
David H <davidhao3300@yahoo.com>
2014-09-18 11:54:43 -0400

    Error catching and rendering.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class BadRequestError < StandardError; end
+
+  def render(options = nil, extra_options = {}, &block)
+    # Add success
+    if options[:json].present? && options[:json][:success].nil?
+      options[:json][:success] = true
+    end
+
+    super(options, extra_options, &block)
+  end
+
+  rescue_from StandardError do |e|
+    render json: { success: false, message: e.message }
+  end
 end
