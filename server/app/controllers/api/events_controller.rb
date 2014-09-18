e1654f2168a2a907007bce67890e40b101c2f5c1
David H <davidhao3300@yahoo.com>
2014-09-18 12:04:02 -0400

    Check for validity of params.

--- server/app/controllers/api/events_controller.rb
+++ server/app/controllers/api/events_controller.rb
@@ -33,9 +33,16 @@ class Api::EventsController < ApplicationController
 
   private
   def check_params
+    # Numerical params check
     [:latitude, :longitude].each do |param|
       if params[param].nil?
         raise BadRequestError, "Missing parameter: #{param}"
+      end
+
+      # Check if param is numeric
+      # TODO: Make this a library method
+      if params[param].to_f.to_s != params[param] && params[param].to_i.to_s != params[param]
+        raise BadRequestError, "Parameter #{param} is not a number"
       end
     end
   end
