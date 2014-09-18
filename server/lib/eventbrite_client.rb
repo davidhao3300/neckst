5bfa643244af34998a956caff85daf25420a7553
David H <davidhao3300@yahoo.com>
2014-09-18 12:36:20 -0400

    Edit Eventbrite client.

--- server/lib/eventbrite_client.rb
+++ server/lib/eventbrite_client.rb
@@ -1,38 +1,15 @@
 require 'httparty'
 class EventbriteClient
   include HTTParty
-  base_uri 'https://www.eventbriteapi.com'
-
-  def initialize( auth_tokens )
-    @auth = {}
-    @data_type = (auth_tokens.is_a?(Hash) and auth_tokens.include? :data_type) ? auth_tokens[:data_type] : 'json'
-    if auth_tokens.is_a? Hash
-      if auth_tokens.include? :access_token
-        # use oauth2 authentication tokens
-        self.class.headers 'Authorization' => "Bearer #{auth_tokens[:access_token]}"
-      elsif auth_tokens.include? :app_key
-        #use api_key OR api_key + user_key OR api_key+email+pass
-        if auth_tokens.include? :user_key
-          # read/write access on the user account associated with :user_key
-          @auth = {:app_key => auth_tokens[:app_key], :user_key => auth_tokens[:user_key]}
-        elsif auth_tokens.include?(:user) && auth_tokens.include?(:password)
-          # read/write access on the user account matching this login info 
-          @auth = {:app_key => auth_tokens[:app_key], :user => auth_tokens[:user], :password => auth_tokens[:password]}
-        else
-          # read-only access to public data
-          @auth = {:app_key => auth_tokens[:app_key]}
-        end
-      end 
-    end
-  end
+  base_uri 'https://www.eventbriteapi.com/v3'
 
   # available API request methods are documented at:
   # http://developer.eventbrite.com/doc
-  def method_request( method, params )
+  def method_request( method, params={} )
     #merge auth params into our request querystring
-    querystring = @auth.merge( params.is_a?(Hash) ? params : {} )
+    params.merge!(token: ENV['EVENTBRITE_TOKEN'])
     method_header = method.to_s.split('_')
-    resp = self.class.get("/v3/#{method_header.first}/#{method_header.last}",{:query => querystring})
+    resp = self.class.get("/#{method_header.first}/#{method_header.last}/", {:query => params})
 
     if resp['error']
       raise RuntimeError, resp['error']['error_message'], caller[1..-1]
